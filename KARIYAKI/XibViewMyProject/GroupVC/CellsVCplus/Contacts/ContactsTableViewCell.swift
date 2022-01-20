107014e5d253ebf1dd449d69b68b4b6d19a76c52
YuryLamaka <loko555@yahoo.com>
2022-01-20 15:02:23 +0300

    add hyperLink to contactsCell

--- KARIYAKI/XibViewMyProject/GroupVC/CellsVCplus/Contacts/ContactsTableViewCell.swift
+++ KARIYAKI/XibViewMyProject/GroupVC/CellsVCplus/Contacts/ContactsTableViewCell.swift
@@ -19,5 +19,35 @@ class ContactsTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    @IBAction func twitterLinkButton(_ sender: Any) {
+        guard let url = URL(string: "https://twitter.com/skiSkateSky") else {
+            return
+        }
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:]) {_ in }
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    @IBAction func linkedinLinkButton(_ sender: Any) {
+        guard let url = URL(string: "https://www.linkedin.com/in/yury-lamaka-69873622b") else {
+            return
+        }
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:]) {_ in }
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    @IBAction func githubLinkButton(_ sender: Any) {
+        guard let url = URL(string: "https://github.com/YuryLamaka") else {
+            return
+        }
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:]) {_ in }
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
     
 }
