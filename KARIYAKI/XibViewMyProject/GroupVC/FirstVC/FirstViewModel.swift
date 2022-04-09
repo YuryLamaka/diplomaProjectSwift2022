daf4e56f9931847ef3bb7565fa37f8d879ef18e8
YuryLamaka <loko555@yahoo.com>
2022-04-09 20:38:49 +0300

    qwe

---
+++ KARIYAKI/XibViewMyProject/GroupVC/FirstVC/FirstViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewModel.swift
+//  diplomaProjectXcode
+//
+//  Created by Yury Lamaka on 9.04.22.
+//
+
+import Foundation
+
+protocol FirstViewProtocol {
+    
+}
+
+class FirstViewModel {
+    
+}
+
+extension FirstViewModel: FirstViewProtocol {
+    
+}
