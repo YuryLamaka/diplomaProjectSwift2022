b95f9ce1b1bc11a7e7bc235e048b7c4067eaeb28
YuryLamaka <loko555@yahoo.com>
2022-04-11 19:50:43 +0300

    it work -> do not delete SceneDelegate

---
+++ KARIYAKI/XibViewMyProject/GroupViewController/AppCoordinator/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  XibViewMyProject
+//
+//  Created by Yury Lamaka on 20.11.21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
+
